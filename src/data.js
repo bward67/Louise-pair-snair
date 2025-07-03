ed1932de6d21d918228f8433105f01da69ba589b
Barbara <subwaynaas@yahoo.ie>
2025-07-03 08:56:15 +0200

    Add full project

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+  { name: "The Invincible Icons", image: "all-22.JPEG" },
+  { name: "The Brat Pack", image: "all.JPEG" },
+  { name: "Dynamic Desi", image: "desi-curling.JPEG" },
+  { name: "Dazzling Desi", image: "desi-cute.JPEG" },
+  { name: "Dauntless Desi", image: "desi.JPEG" },
+  { name: "The Three Monkeys", image: "elkridge_sign.JPEG" },
+  { name: "Fearless Frances", image: "frances-cute.JPEG" },
+  { name: "Funky Frances", image: "frances.JPEG" },
+  { name: "Gigawatt Gavin", image: "gavin-hockey2.JPEG" },
+  { name: "Gutsy Gavin", image: "gavin-winter.JPEG" },
+  { name: "Glorious Gavin", image: "gavin.JPEG" },
+  { name: "Jazzy Aunty Jen / Mega Mom", image: "jen.JPEG" },
+  { name: "Sparky Simon", image: "simon-box.JPEG" },
+  { name: "Supersonic Simon", image: "simon-camping.JPEG" },
+  { name: "Superman Simon", image: "simon-game.JPEG" },
+];
+
+export default data;
---
+++ src/images.js
@@ -0,0 +1,37 @@
+import All2 from "./assets/all-22.JPEG";
+import All from "./assets/all.JPEG";
+import DesiCurling from "./assets/desi-curling.JPEG";
+import DesiCute from "./assets/desi-cute.JPEG";
+import Desi from "./assets/desi.JPEG";
+import ElkridgeSign from "./assets/elkridge_sign.JPEG";
+import FrancesCute from "./assets/frances-cute.JPEG";
+import Frances from "./assets/frances.JPEG";
+import GavinHockey2 from "./assets/gavin-hockey2.JPEG";
+import GavinWinter from "./assets/gavin-winter.JPEG";
+import Gavin from "./assets/gavin.JPEG";
+import Jen from "./assets/jen.JPEG";
+import Louise from "./assets/louise.JPEG";
+import SimonBox from "./assets/simon-box.JPEG";
+import SimonCamping from "./assets/simon-camping.JPEG";
+import SimonGame from "./assets/simon-game.JPEG";
+
+const images = {
+  "all-22.JPEG": All2,
+  "all.JPEG": All,
+  "desi-curling.JPEG": DesiCurling,
+  "desi-cute.JPEG": DesiCute,
+  "desi.JPEG": Desi,
+  "elkridge_sign.JPEG": ElkridgeSign,
+  "frances-cute.JPEG": FrancesCute,
+  "frances.JPEG": Frances,
+  "gavin-hockey2.JPEG": GavinHockey2,
+  "gavin-winter.JPEG": GavinWinter,
+  "gavin.JPEG": Gavin,
+  "jen.JPEG": Jen,
+  "louise.JPEG": Louise,
+  "simon-box.JPEG": SimonBox,
+  "simon-camping.JPEG": SimonCamping,
+  "simon-game.JPEG": SimonGame,
+};
+
+export default images;
